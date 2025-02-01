b0bd7921c930a6c6b775fa0d82b71c113127e4d2
vedpatel-12 <vedptl@gmail.com>
2025-02-01 13:21:31 -0500

    Revert "frontend commit"
    This reverts commit 8132f9991a8094601281b155e2edd2b485b8743a.

--- src/pages/Donate.jsx
+++
@@ -1,9 +0,0 @@
-export default function Donate() {
-    return (
-      <div style={{ textAlign: "center", padding: "50px" }}>
-        <h1>Donate an Item</h1>
-        <p>Upload a photo and select an organization.</p>
-        {/* We'll add the donation form here later */}
-      </div>
-    );
-  }
--- src/pages/Organizations.jsx
+++
@@ -1,9 +0,0 @@
-export default function Organizations() {
-    return (
-      <div style={{ textAlign: "center", padding: "50px" }}>
-        <h1>Organizations</h1>
-        <p>Browse organizations in our network.</p>
-        {/* We'll fetch organization data from the backend later */}
-      </div>
-    );
-  }
